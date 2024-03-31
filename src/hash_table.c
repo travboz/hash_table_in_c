52605e579ec566c57436e3da56f3b6d1318ccfeb
Travis Bozic <z5368318@ad.unsw.edu.au>
2024-03-31 21:18:30 +1100

    Fix up error of deallocating unallocated memory and restructure .h and .c API files

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,18 +4,6 @@
 
 #include "hash_table.h"
 
-/*
-This function allocates a chunk of memory the size of an ht_item, and saves
-a copy of the string `k` and `v` in the new chunk of memory.
-It is static because it is only ever used internally by the hash table.
-
-Input:
-    const char* k: a string as the key
-    const char* v: a string as the associated value to key
-
-Output:
-    ht_item: pointer to the location of the newly created ht_item
-*/
 static ht_item *ht_new_item(const char *k, const char *v)
 {
     // we make a pointer the size of an item
@@ -25,18 +13,6 @@ static ht_item *ht_new_item(const char *k, const char *v)
     return i;
 }
 
-/*
-Initialises a new hash table.
-`size` defines how many items we can store in this hash table.
-We initialise the `items` empty array with calloc to be filled with NULL bytes.
-Where a NULL entry indicates that the bucket (that location) is empty.
-
-Input:
-    None
-
-Output:
-    ht_hash_table: pointer to the location of the newly created hash_table.
-*/
 ht_hash_table *ht_new()
 {
     ht_hash_table *ht = malloc(sizeof(ht_hash_table));
@@ -44,36 +20,10 @@ ht_hash_table *ht_new()
     ht->size = 53;
     ht->count = 0;
     ht->items = calloc((size_t)ht->size, sizeof(ht_item *));
+
+    return ht;
 }
 
-/*
-Deletes the specified item pointed to by the argument.
-This is static as it is used internally by ht_hash_table.
-
-Input:
-    ht_item *i: item to delete
-
-Output:
-    None
-*/
-static void ht_del_item(ht_item *i)
-{
-    free(i->key);
-    free(i->value);
-    free(i);
-}
-
-/*
-Deletes an entire hash table by systematically freeing all necessary
-items first and then freeing the reference to the items array,
-and finally by freeing the table reference passed in.
-
-Input:
-    ht_hash_table *ht: hash table to delete
-
-Output:
-    None
-*/
 void ht_del_hash_table(ht_hash_table *ht)
 {
     for (int i = 0; i < ht->size; i++)
@@ -88,4 +38,11 @@ void ht_del_hash_table(ht_hash_table *ht)
 
     free(ht->items);
     free(ht);
+}
+
+static void ht_del_item(ht_item *i)
+{
+    free(i->key);
+    free(i->value);
+    free(i);
 }
--- src/hash_table.h
+++ src/hash_table.h
@@ -1,4 +1,6 @@
 // hash_table.h
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
 
 // our key-value pairs: items
 typedef struct
@@ -13,3 +15,58 @@ typedef struct
     int count;
     ht_item **items; // array of pointers to items
 } ht_hash_table;
+
+/*
+This function allocates a chunk of memory the size of an ht_item, and saves
+a copy of the string `k` and `v` in the new chunk of memory.
+It is static because it is only ever used internally by the hash table.
+
+Input:
+    const char* k: a string as the key
+    const char* v: a string as the associated value to key
+
+Output:
+    ht_item: pointer to the location of the newly created ht_item
+*/
+static ht_item *ht_new_item(const char *k, const char *v);
+
+/*
+Initialises a new hash table.
+`size` defines how many items we can store in this hash table.
+We initialise the `items` empty array with calloc to be filled with NULL bytes.
+Where a NULL entry indicates that the bucket (that location) is empty.
+
+Input:
+    None
+
+Output:
+    ht_hash_table: pointer to the location of the newly created hash_table.
+*/
+ht_hash_table *ht_new();
+
+/*
+Deletes an entire hash table by systematically freeing all necessary
+items first and then freeing the reference to the items array,
+and finally by freeing the table reference passed in.
+
+Input:
+    ht_hash_table *ht: hash table to delete
+
+Output:
+    None
+*/
+void ht_del_hash_table(ht_hash_table *ht);
+
+/*
+Deletes the specified item pointed to by the argument.
+This is static as it is used internally by ht_hash_table.
+
+Input:
+    ht_item *i: item to delete
+
+Output:
+    None
+*/
+static void ht_del_item(ht_item *i);
+
+#endif /* HASH_TABLE_H */
